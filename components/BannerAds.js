1e5c5bdedcfe3f8e7668db8e32eeb456ccaea2c5
Ufuk <ufukdemirg@gmail.com>
2023-10-24 20:20:57 +0300

    24.10

--- components/BannerAds.js
+++ components/BannerAds.js
@@ -2,14 +2,14 @@ import { View, Text, Button} from 'react-native'
 import React from 'react'
 import {BannerAd, BannerAdSize, TestIds,InterstitialAd,AdEventType} from 'react-native-google-mobile-ads';
 
-const adUnitId = __DEV__ ? TestIds.APP_OPEN : 'ca-app-pub-9042490279958793/3673351850';
+const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-9042490279958793/3673351850';
 
 const BannerAds = () => {
   return (
     <View style={{alignItems:"center"}}>
       <BannerAd
         size={BannerAdSize.BANNER}
-        unitId={TestIds.BANNER}
+        unitId={adUnitId}
       />
     </View>
   )
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,7 +15,8 @@ const HomeScreen = ({ navigation }) => {
     return (
         <SafeAreaView style={styles.container}>
             <ImageBackground source={require("../../assets/images/backpng.png")} resizeMode='stretch' style={{ flex: 1, gap: 30 }}>
-                <View style={{ flex: 2, justifyContent: "top", alignItems: "center", padding: 20 }}><Text style={{ fontSize: 30, fontFamily: "Koblenz-Serial-Heavy", color: "#333e54" }}>AKTÜEL ÜRÜNLER</Text></View>
+                <View style={{ flex: 2, justifyContent: "top", alignItems: "center", padding: 20 }}>
+                    <Text style={{ fontSize: 30, fontFamily: "Koblenz-Serial-Heavy", color: "#feede6" }}>AKTÜEL ÜRÜNLER</Text></View>
 
                 <TouchableOpacity style={[styles.touchableOpacityStyle, { backgroundColor: "#ed1c24" }]}
                     onPress={() => navigation.navigate(BimBottomTabs)}>
@@ -34,7 +35,9 @@ const HomeScreen = ({ navigation }) => {
                     <Image style={styles.imageStyle} source={require("../../assets/images/soklogo.png")}></Image>
                 </TouchableOpacity><View style={{ flex: 0.2 }}></View>
             </ImageBackground>
-            <BannerAds />
+            <View style={{ position: "absolute" ,bottom:0, alignSelf:"center"}}>
+                <BannerAds/>
+            </View>
         </SafeAreaView>
     )
 }
